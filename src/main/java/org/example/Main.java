278f76a7f8ffeebd642d022fba155b71395c80e1
HmzhDubh <meezo.2015@hotmail.com>
2023-01-23 00:12:12 +0300

    i added a DB friver and connected it in the main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
-
+import java.sql.*;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         // Init page to open either sign Up or Login page
         InitialPage initFrame = new InitialPage();
@@ -13,5 +13,17 @@ public class Main {
         initFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initFrame.setResizable(false);
 
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection con= DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe","system","IVIeeZo_2015");
+
+        Statement stmt=con.createStatement();
+
+        ResultSet rs=stmt.executeQuery("select * from breachyAppTest");
+        while(rs.next()){
+
+            System.out.println(rs.getString("username")+rs.getString("password"));
+        }
+        con.close();
     }
 }
