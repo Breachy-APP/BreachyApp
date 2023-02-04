494319113b1f10d43a11ed7ce836e6bff577d189
Baraa <baraabashammakh@outlook.com>
2023-02-04 23:02:43 +0300

    Adding virus scan feature

--- src/main/java/org/breachy/Main.java
+++ src/main/java/org/breachy/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         // Init page to open either sign Up or Login page
         InitialPage initFrame = new InitialPage();
-        initFrame.openIntialPage(initFrame, true);
+       initFrame.openIntialPage(initFrame, true);
+
     }
 }
